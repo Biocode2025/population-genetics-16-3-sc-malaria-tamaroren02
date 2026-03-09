0417128c4c34426a416d082ea629a3f69c6ce56f
tamaroren02 <tamaroren0211@gmail.com>
2026-03-09 15:32:19 +0200

    פתיחת לולאה וחישוה נתונים לדור הנוחכי והדפסתם לטבלה.

--- scripts/main.py
+++ scripts/main.py
@@ -5,3 +5,9 @@ Freq_q=0.0036**0.5 #שכיחות האלל HbS (q)
 gen=10
 generation=0
 Sickle_cell_freq_het.write("generation\tFreq_HbA\tFreq_HbS\tFreq_HbAHbA\tFreq_HbAHbS\tFreq_HbSHbS\n")#שורה ראשונה של הטבלה
+while gen > 0 :
+    Freq_p = 1 - Freq_q #שכיחויות בדור הנוכחי
+    Freq_pp = Freq_p**2 
+    Freq_pq = 2*Freq_p*Freq_q
+    Freq_qq = Freq_q**2
+    Sickle_cell_freq_het.write("%d\t%.6f\t%.6f\t%.6f\t%.6f\t%.6f\n" %(generation, Freq_p, Freq_q, Freq_pp, Freq_pq, Freq_qq))#כתיבה לקובץ
